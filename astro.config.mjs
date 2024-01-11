ec34c91dabdc399a3d40016adaa802dfaeedbb70
jojeho <whwogh@hanmail.net>
2024-01-11 03:50:54 +0000

    aa

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,15 @@ import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 import htmx from "astro-htmx";
 import node from "@astrojs/node";
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  // output: "server",
+  
   integrations: [preact(), htmx()],
   adapter: node({
    mode: "standalone"
    })
   ,
-
+  adapter: netlify(),
 });
