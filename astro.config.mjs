f04a8053315773da517b60a383084839a5eb025d
jojeho <whwogh@hanmail.net>
2024-01-11 03:34:12 +0000

    no ouput

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  // output: "server",
   integrations: [preact(), htmx()],
   adapter: node({
    mode: "standalone"
