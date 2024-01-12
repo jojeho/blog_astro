47a6a9b066287e65ee6da8ca551bc298794f399d
jojeho <whwogh@hanmail.net>
2024-01-12 21:52:38 +0000

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import preact from "@astrojs/preact";
 import htmx from "astro-htmx";
 import node from "@astrojs/node";
 import netlify from '@astrojs/netlify';
+import stylexIntegration from "@sfroment/astro-stylex";
+
 
 
 export default defineConfig({
   output: "server",
-  integrations: [preact(), htmx()],
+  integrations: [preact(), htmx() ,stylexIntegration  ()],
   adapter: node({
    mode: "standalone"
    })
--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -1,17 +1,20 @@
 import { useState } from 'preact/hooks';
 import "../styles/global.css"
 
+
 export default function Greeting({messages}:{messages:string[]}) {
   
   const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
   const [greeting, setGreeting] = useState(messages[0]);
 
   return (
-    <div>
-      <h3>{greeting}! Thank you for visiting!</h3>
+    
+   <div >
+      <p>{greeting}! Thank you for visiting!</p>
       <button onClick={() => setGreeting(randomMessage())}>
         New Greeting
       </button>
     </div>
+  
   );
 }
